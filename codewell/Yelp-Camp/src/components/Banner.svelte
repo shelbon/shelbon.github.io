702d2ade04fc95310fc1642a090fa0b790a89263
shelbon <mouclepatrick@protonmail.com>
2022-01-12 17:31:33 +0100

    ✨feature: Add landing page

---
+++ codewell/Yelp-Camp/src/components/Banner.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let bannerItems = [];
+</script>
+
+<ul class="flex ">
+	{#each bannerItems as bannerItem}
+		<li class="item">
+			<img src={bannerItem.src} alt="logo of {bannerItem.name}" />
+		</li>
+	{/each}
+</ul>
---
+++ codewell/Yelp-Camp/src/components/Features.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<ul class="space-y-4 lg:space-y-12">
+	<li class="relative before:content-[url(/assets/Checkmark.svg)] before:absolute before:left-0">
+		<p class="pl-12 opacity-70 md:text-3xl">Add your own camp suggestions.</p>
+	</li>
+	<li class="relative before:content-[url(/assets/Checkmark.svg)] before:absolute before:left-0">
+		<p class="pl-12 opacity-70 md:text-3xl">Leave reviews and experiences.</p>
+	</li>
+	<li class="relative before:content-[url(/assets/Checkmark.svg)] before:absolute before:left-0">
+		<p class="pl-12 opacity-70 md:text-3xl">See locations for all camps.</p>
+	</li>
+</ul>
